72d4f8962ee47cf412ddaeedbbdc53cb984430f6
dubaiincairo <info@dubaiincairo.com>
2026-04-28 19:12:33 +0300

    Add Sanity page schema

---
+++ src/sanity/schemaTypes/pageType.ts
@@ -0,0 +1,34 @@
+import { defineField, defineType } from "sanity";
+
+export const pageType = defineType({
+  name: "page",
+  title: "Page",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title" },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+      rows: 3,
+    }),
+    defineField({
+      name: "body",
+      title: "Body",
+      type: "array",
+      of: [{ type: "block" }],
+    }),
+  ],
+});
